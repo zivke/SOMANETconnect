350dc3d3186e0f59f042828a6b2038fb95decac7
Nikola Zivkovic <zivke85@gmail.com>
2015-03-26 16:28:28 +0100

    Add the SystemProcessLive class used for real-time sending of a process output to the client side of a web socket

--- src/main/java/SOMANETconnect/SomanetServer.java
+++ src/main/java/SOMANETconnect/SomanetServer.java
@@ -16,15 +16,21 @@ import org.java_websocket.server.WebSocketServer;
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SomanetServer extends WebSocketServer {
 
     private final static Logger logger = Logger.getLogger(SomanetServer.class.getName());
 
+    private Map<String, WebSocket> activeRequestRegister;
+
     @Inject
     public SomanetServer(Configuration applicationConfiguration, SSLContext sslContext) throws Exception {
         super(new InetSocketAddress(applicationConfiguration.getInt("application.ws.port")));
         setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
+
+        this.activeRequestRegister = new HashMap<>();
     }
 
     @Override
@@ -38,10 +44,10 @@ public class SomanetServer extends WebSocketServer {
     }
 
     @Override
-    public void onMessage(WebSocket conn, String message) {
+    public void onMessage(WebSocket webSocketConnection, String message) {
         // Parse request string
         JSONRPC2Request request;
-        JSONRPC2Response response = null;
+        JSONRPC2Response response;
 
         try {
             request = JSONRPC2Request.parse(message);
@@ -55,17 +61,20 @@ public class SomanetServer extends WebSocketServer {
                 case Constants.LIST:
                     ListCommand listCommand = new ListCommand();
                     response = new JSONRPC2Response(listCommand.getDeviceList(), request.getID());
+                    webSocketConnection.send(response.toString());
+                    break;
+                case Constants.FLASH:
+                    String requestId = (String) request.getID();
+                    activeRequestRegister.put(requestId, webSocketConnection);
+                    (new Thread(new SystemProcessLive("./testLive.sh", activeRequestRegister, requestId))).start();
                     break;
                 default:
                     response = new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, request.getID());
+                    webSocketConnection.send(response.toString());
             }
         } catch (IOException e) {
             response = new JSONRPC2Response(JSONRPC2Error.INTERNAL_ERROR, request.getID());
-        } finally {
-            if (response == null) {
-                response = new JSONRPC2Response(JSONRPC2Error.INTERNAL_ERROR, request.getID());
-            }
-            conn.send(response.toString());
+            webSocketConnection.send(response.toString());
         }
     }
 
---
+++ src/main/java/SOMANETconnect/SystemProcessLive.java
@@ -0,0 +1,93 @@
+package SOMANETconnect;
+
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import org.apache.log4j.Logger;
+import org.java_websocket.WebSocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Used for asynchronous process execution that send each line of the process output to the client on the other side of
+ * the web socket.
+ */
+public class SystemProcessLive implements Runnable {
+
+    private static final Logger logger = Logger.getLogger(SystemProcessLive.class.getName());
+
+    class StreamReader extends Thread {
+        InputStream is;
+        String requestId;
+        WebSocket webSocketConnection;
+
+        // Reads everything from the input stream until it is empty
+        StreamReader(InputStream is, String requestId, WebSocket webSocketConnection) {
+            this.is = is;
+            this.requestId = requestId;
+            this.webSocketConnection = webSocketConnection;
+        }
+
+        public void run() {
+            try {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                JSONRPC2Response response;
+                while ((line = br.readLine()) != null) {
+                    Map<String, String> outputLine = new HashMap<>();
+                    outputLine.put(Constants.EXEC_LIVE, line);
+                    response = new JSONRPC2Response(outputLine, requestId);
+                    webSocketConnection.send(response.toString());
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+    }
+
+    private String command;
+    private String requestId;
+    private Map<String, WebSocket> activeRequestRegister;
+
+    public SystemProcessLive(String command, Map<String, WebSocket> activeRequestRegister, String requestId)
+            throws IOException {
+        this.command = command;
+        this.requestId = requestId;
+        this.activeRequestRegister = activeRequestRegister;
+    }
+
+    @Override
+    public void run() {
+        ProcessBuilder processBuilder = new ProcessBuilder().command(command).redirectErrorStream(true);
+        Process process;
+        try {
+            process = processBuilder.start();
+        } catch (IOException e) {
+            logger.error("An I/O error occurred during process executing (Command: " + command + "; Request ID: "
+                    + requestId + ")");
+            activeRequestRegister.remove(requestId);
+            return;
+        }
+
+        WebSocket webSocketConnection = activeRequestRegister.get(requestId);
+
+        StreamReader outputReader = new StreamReader(process.getInputStream(), requestId, webSocketConnection);
+        outputReader.start();
+
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            // NO-OP
+        }
+
+        JSONRPC2Response response = new JSONRPC2Response(Constants.EXEC_DONE, requestId);
+        webSocketConnection.send(response.toString());
+
+        activeRequestRegister.remove(requestId);
+    }
+}
