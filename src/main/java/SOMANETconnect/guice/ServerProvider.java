9639de0ac22385400fb97d5ac065b911ecc3c650
Nikola Zivkovic <zivke85@gmail.com>
2015-04-01 13:24:57 +0200

    Enable secure connecting to port 62523

--- src/main/java/SOMANETconnect/guice/ServerProvider.java
+++ src/main/java/SOMANETconnect/guice/ServerProvider.java
@@ -4,9 +4,9 @@ import SOMANETconnect.SomanetWebSocketAdapter;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import org.apache.commons.configuration.Configuration;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
@@ -17,9 +17,29 @@ public class ServerProvider implements Provider<Server> {
     @Override
     public Server get() {
         Server server = new Server();
+
+        // Normal web socket connection (ws)
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(applicationConfiguration.getInt("application.ws.port"));
         server.addConnector(connector);
+
+        // Secure (SSL) web socket connection (wss)
+        HttpConfiguration https = new HttpConfiguration();
+        https.addCustomizer(new SecureRequestCustomizer());
+
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(
+                SomanetWebSocketAdapter.class.getResource(
+                        applicationConfiguration.getString("application.ws.ssl.key_store_path")).getPath());
+        sslContextFactory.setKeyStorePassword(applicationConfiguration.getString("application.ws.ssl.key_store_password"));
+        sslContextFactory.setKeyManagerPassword(applicationConfiguration.getString("application.ws.ssl.key_manager_password"));
+
+        ServerConnector sslConnector = new ServerConnector(server,
+                new SslConnectionFactory(sslContextFactory, "http/1.1"),
+                new HttpConnectionFactory(https));
+        sslConnector.setPort(applicationConfiguration.getInt("application.ws.ssl.port"));
+
+        server.addConnector(sslConnector);
 
         // Setup the basic application "context" for this application at "/" and a web socket handler
         ContextHandler context = new ContextHandler();
