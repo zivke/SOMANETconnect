ea450df4f00608cdefaa717b4c8a0c4c4678107a
Nikola Zivkovic <zivke85@gmail.com>
2015-04-22 11:12:53 +0200

    Load the keystore file as a stream because of the problems that happen when its path is used (when the application is ran as a JAR file)

--- src/main/java/SOMANETconnect/guice/OblacServerProvider.java
+++ src/main/java/SOMANETconnect/guice/OblacServerProvider.java
@@ -4,13 +4,22 @@ import SOMANETconnect.OblacWebSocketAdapter;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import org.apache.commons.configuration.Configuration;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 public class OblacServerProvider implements Provider<Server> {
+    private final static Logger logger = Logger.getLogger(OblacServerProvider.class.getName());
+
     @Inject
     private Configuration applicationConfiguration;
 
@@ -28,11 +37,19 @@ public class OblacServerProvider implements Provider<Server> {
         https.addCustomizer(new SecureRequestCustomizer());
 
         SslContextFactory sslContextFactory = new SslContextFactory();
-        sslContextFactory.setKeyStorePath(
-                OblacWebSocketAdapter.class.getResource(
-                        applicationConfiguration.getString("application.ws.ssl.key_store_path")).getPath());
-        sslContextFactory.setKeyStorePassword(applicationConfiguration.getString("application.ws.ssl.key_store_password"));
-        sslContextFactory.setKeyManagerPassword(applicationConfiguration.getString("application.ws.ssl.key_manager_password"));
+        try {
+            KeyStore keyStore = KeyStore.getInstance("jks");
+            // Load the keystore file as a stream because of the problems that happen when its path is used (when the
+            // application is ran as a JAR file)
+            keyStore.load(
+                    OblacServerProvider.class.getResourceAsStream(
+                            applicationConfiguration.getString("application.ws.ssl.key_store_path")),
+                    applicationConfiguration.getString("application.ws.ssl.key_store_password").toCharArray());
+            sslContextFactory.setKeyStore(keyStore);
+            sslContextFactory.setKeyStorePassword(applicationConfiguration.getString("application.ws.ssl.key_store_password"));
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e) {
+            logger.error(e.getMessage());
+        }
 
         ServerConnector sslConnector = new ServerConnector(server,
                 new SslConnectionFactory(sslContextFactory, "http/1.1"),
