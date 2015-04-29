105b9250ce9e8fa0a2e0d77e5c842ed4af5f8c38
Nikola Zivkovic <zivke85@gmail.com>
2015-04-29 19:03:20 +0200

    Refactor

--- src/main/java/SOMANETconnect/SomanetConnectSystemTray.java
+++ src/main/java/SOMANETconnect/SomanetConnectSystemTray.java
@@ -17,21 +17,83 @@ import java.util.Map;
 public class SomanetConnectSystemTray {
     private static final Logger logger = Logger.getLogger(SomanetConnectSystemTray.class.getName());
 
+    private TrayIcon trayIcon;
+    private JPopupMenuEx popupMenu;
     private ArrayList<JMenuItem> currentDeviceMenuItems = new ArrayList<>();
+
+    /**
+     * A worker class that finds all available devices and puts them into the popup menu
+     */
+    private class Worker implements Runnable {
+        @Override
+        public void run() {
+            ListCommand listCommand;
+            try {
+                listCommand = new ListCommand();
+            } catch (IOException e2) {
+                logger.error(e2.getMessage());
+                return;
+            }
+            java.util.List devices = listCommand.getDeviceList();
+
+            // Remove current device labels
+            for (JMenuItem deviceMenuItem : currentDeviceMenuItems) {
+                popupMenu.remove(deviceMenuItem);
+            }
+            currentDeviceMenuItems.clear();
+
+            if (devices.isEmpty()) {
+                JMenuItem noAvailableDevicesMenuItem = new JMenuItem("No available devices");
+                popupMenu.insert(noAvailableDevicesMenuItem, currentDeviceMenuItems.size());
+
+                currentDeviceMenuItems.add(noAvailableDevicesMenuItem);
+            } else {
+                for (Object deviceObject : devices) {
+                    Map device = (Map) deviceObject;
+                    String tileString = (String) device.get(Constants.DEVICES);
+                    JMenuItem deviceMenuItem;
+                    if (tileString.equalsIgnoreCase("in use")) {
+                        deviceMenuItem = new JMenuItem(
+                                "Device in use    (Adapter: " + device.get(Constants.ADAPTER_ID) + ")");
+                    } else {
+                        tileString = tileString.substring(5, tileString.length() - 1);
+                        int tileNumber = Integer.valueOf(tileString) + 1;
+                        deviceMenuItem = new JMenuItem(
+                                tileNumber + " tile device    (Adapter: " + device.get(Constants.ADAPTER_ID) + ")");
+                    }
+                    popupMenu.insert(deviceMenuItem, currentDeviceMenuItems.size());
+
+                    currentDeviceMenuItems.add(deviceMenuItem);
+                }
+            }
+            // Resize the ancestor window of the popup menu to the required size
+            Window window = SwingUtilities.getWindowAncestor(popupMenu);
+            if (window != null) {
+                window.pack();
+                window.validate();
+            }
+        }
+    }
 
     public SomanetConnectSystemTray() throws IOException {
         //Check the SystemTray is supported
         if (!SystemTray.isSupported()) {
-            System.out.println("SystemTray is not supported");
+            logger.error("SystemTray is not supported");
             return;
         }
 
+        // Set the default styling of the application to match that of the system's
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.warn(e.getMessage());
         }
 
+        initPopupMenu();
+        initTrayIcon();
+    }
+
+    private static Image getIconImage() throws IOException {
         BufferedImage bufferedImage;
         // The system tray icon cannot be transparent in Linux
         if (SystemUtils.IS_OS_LINUX) {
@@ -40,74 +102,14 @@ public class SomanetConnectSystemTray {
             bufferedImage =
                     ImageIO.read(SomanetConnect.class.getResourceAsStream("/synapticon_tray_icon_transparent.png"));
         }
-        final SystemTray systemTray = SystemTray.getSystemTray();
-        Dimension trayIconSize = systemTray.getTrayIconSize();
-        Image image = bufferedImage.getScaledInstance(trayIconSize.width - 2, trayIconSize.height, Image.SCALE_SMOOTH);
-        final TrayIcon trayIcon = new TrayIcon(image);
+        Dimension trayIconSize = SystemTray.getSystemTray().getTrayIconSize();
+        return bufferedImage.getScaledInstance(trayIconSize.width - 2, trayIconSize.height, Image.SCALE_SMOOTH);
+    }
 
-        final JPopupMenuEx jPopupMenu = new JPopupMenuEx();
+    private void initPopupMenu() {
+        popupMenu = new JPopupMenuEx();
 
-        trayIcon.addMouseListener(new MouseAdapter() {
-            public void mouseReleased(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON3) {
-                    jPopupMenu.setLocation(e.getX(), e.getY());
-                    jPopupMenu.setInvoker(jPopupMenu);
-                    jPopupMenu.setVisible(true);
-
-                    // Run the listing process in a separate thread, so that the context menu doesn't lag
-                    (new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            ListCommand listCommand;
-                            try {
-                                listCommand = new ListCommand();
-                            } catch (IOException e2) {
-                                logger.error(e2.getMessage());
-                                return;
-                            }
-                            java.util.List devices = listCommand.getDeviceList();
-
-                            // Remove current device labels
-                            for (JMenuItem deviceMenuItem : currentDeviceMenuItems) {
-                                jPopupMenu.remove(deviceMenuItem);
-                            }
-                            currentDeviceMenuItems.clear();
-
-                            if (devices.isEmpty()) {
-                                JMenuItem noAvailableDevicesMenuItem = new JMenuItem("No available devices");
-                                jPopupMenu.insert(noAvailableDevicesMenuItem, currentDeviceMenuItems.size());
-
-                                currentDeviceMenuItems.add(noAvailableDevicesMenuItem);
-                            } else {
-                                for (Object deviceObject : devices) {
-                                    Map device = (Map) deviceObject;
-                                    String tileString = (String) device.get(Constants.DEVICES);
-                                    JMenuItem deviceMenuItem;
-                                    if (tileString.equalsIgnoreCase("in use")) {
-                                        deviceMenuItem = new JMenuItem(
-                                                "Device in use    (Adapter: " + device.get(Constants.ADAPTER_ID) + ")");
-                                    } else {
-                                        tileString = tileString.substring(5, tileString.length() - 1);
-                                        int tileNumber = Integer.valueOf(tileString) + 1;
-                                        deviceMenuItem = new JMenuItem(
-                                                tileNumber + " tile device    (Adapter: " + device.get(Constants.ADAPTER_ID) + ")");
-                                    }
-                                    jPopupMenu.insert(deviceMenuItem, currentDeviceMenuItems.size());
-
-                                    currentDeviceMenuItems.add(deviceMenuItem);
-                                }
-                            }
-                            // Resize the ancestor window of the popup menu to the required size
-                            Window window = SwingUtilities.getWindowAncestor(jPopupMenu);
-                            window.pack();
-                            window.validate();
-                        }
-                    })).start();
-                }
-            }
-        });
-
-        jPopupMenu.addSeparator();
+        popupMenu.addSeparator();
         JCheckBoxMenuItem startOnBootItem = new JCheckBoxMenuItem("Start on boot");
         startOnBootItem.addItemListener(new ItemListener() {
             @Override
@@ -120,7 +122,7 @@ public class SomanetConnectSystemTray {
             }
         });
         startOnBootItem.setState(Util.isStartOnBootEnabled());
-        jPopupMenu.add(startOnBootItem);
+        popupMenu.add(startOnBootItem);
 
         JMenuItem aboutItem = new JMenuItem("About SOMANETconnect");
         aboutItem.addActionListener(new ActionListener() {
@@ -128,7 +130,7 @@ public class SomanetConnectSystemTray {
                 JOptionPane.showMessageDialog(null, "Synapticon SOMANETconnect v1.0", "About", JOptionPane.INFORMATION_MESSAGE);
             }
         });
-        jPopupMenu.add(aboutItem);
+        popupMenu.add(aboutItem);
 
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(new ActionListener() {
@@ -137,12 +139,29 @@ public class SomanetConnectSystemTray {
                 System.exit(0);
             }
         });
-        jPopupMenu.add(exitItem);
+        popupMenu.add(exitItem);
+    }
+
+    private void initTrayIcon() throws IOException {
+        trayIcon = new TrayIcon(getIconImage());
+
+        trayIcon.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON3) {
+                    popupMenu.setLocation(e.getX(), e.getY());
+                    popupMenu.setInvoker(popupMenu);
+                    popupMenu.setVisible(true);
+
+                    // Run the listing process in a separate thread, so that the context menu doesn't lag
+                    (new Thread(new Worker())).start();
+                }
+            }
+        });
 
         try {
-            systemTray.add(trayIcon);
+            SystemTray.getSystemTray().add(trayIcon);
         } catch (AWTException e) {
-            System.out.println("TrayIcon could not be added.");
+            logger.error("TrayIcon could not be added.");
         }
     }
 }
