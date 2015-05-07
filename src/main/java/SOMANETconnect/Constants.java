d9270dbeb16c12c021de8ca93bafda4377adc3be
Nikola Zivkovic <zivke85@gmail.com>
2015-05-07 15:34:50 +0200

    Remove unused constants

--- src/main/java/SOMANETconnect/Constants.java
+++ src/main/java/SOMANETconnect/Constants.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Constants {
-    public final static String ERROR = "error";
     public final static String LIST = "list";
     public final static String FLASH = "flash";
     public final static String RUN = "run";
@@ -19,16 +18,9 @@ public final class Constants {
     public final static String START_MOTOR = "start-motor";
     public final static String STOP_MOTOR = "stop-motor";
     public final static String SEND = "send";
-    public final static String PARAMS = "params";
-    public final static String EXEC_LIVE = "exec_live";
     public final static String EXEC_DONE = "exec_done";
     public final static String ERASE_FIRMWARE = "erase-firmware";
-    public final static String CODE = "code";
-    public final static String MESSAGE = "message";
-    public final static String METHOD = "method";
     public final static String ID = "id";
-    public final static String JSONRPC = "jsonrpc";
-    public final static String JSONRPC_VERSION = "2.0";
     public final static String TYPE = "type";
     public final static String NUMBER = "number";
     public final static String NAME = "name";
@@ -37,8 +29,6 @@ public final class Constants {
     public final static String TIMESTAMP = "timestamp";
     public final static String ADAPTER_ID = "adapter_id";
     public final static String DEVICES = "devices";
-    public final static String CONTENT = "content";
-    public final static String RESULT = "result";
     public final static String MOTOR_PARAMETERS = "motor_parameters";
     public final static Map<String, String> environmentVariables;
 
